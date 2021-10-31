5b98f4a205e793aed8a3fbb100245a45bf2ba456
Ophelie22 <59674073+Ophelie22@users.noreply.github.com>
2021-10-31 20:24:34 +0100

    pas encore fct

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Ton-template
+
+Sur ce projet on va stocker des donnes  qui vont permettre à l'utilisateur de choisir si il veut un mode sombre ou clair d'affichage sur le site.
+
+Lorsqu'on cliquer sur notre bouton, on va appliquer la class dark à notre body, le fond sera alors bleu nuit et notre texte blanc.
+Si la classe 'dark' est déjà présente, ceci veut dire que le thème actuel est le thème sombre. Donc :
+Retirez toutes les classes de l'élément Body
+Changez le texte du bouton grâce à notre élément span en "Thème sombre"
+
+# Utiliser LOCAL STORAGE
+Définissez un élément localStorage, qui s'appellera 'theme' et qui aura pour valeur 'clair'
+Si la classe 'dark' n'est pas déjà présente, alors nous sommes sur le thème clair. Donc :
---
+++ app.js
@@ -0,0 +1,37 @@
+
+//on selectionne notre bouton avec l'id
+let button = document.querySelector('#mode');
+//on selectionne notre span ici pas d'id ou de class
+let span = document.querySelector('span');
+//on va detecter le click de l'/utilisateur et on rajoute une fct anomyme
+
+
+if(localStorage.getItem('theme')){
+  if(localStorage.getItem('theme') == 'sombre') {
+    modeSombre();
+  }
+}
+
+button.addEventListener('click',() => {
+//un tableau de ttes nos class ds l'element body; puis la conditions va m'envoyer vrai si 
+//l'element body a bien une class dark
+//MODE CLAIR
+if(document.body.classList.contains('dark')) {
+document.body.className = '' ;
+span. textContent = 'Theme sombre';
+// On va definir la prefede l'utilsateur en la stockant sur son navigateur
+localStorage.setItem('theme','clair');
+}
+//autrement si mon  elemt body n'a pas de class Dark
+//MODE SOMBRE
+else{
+  modeSombre ();
+}
+});
+function modeSombre () {
+  //je parametre ma class dark
+document.body.className ='dark';
+span.textContent = 'Thème clair';
+localStorage.setItem('theme', 'sombre');
+
+}
